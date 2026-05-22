6f6a9d9c8123131024bfa1ba89fe0283c65154b4
KosukeHiramatsu <kosuke.h92@gmail.com>
2026-05-22 17:20:22 +0900

    5/22完了分

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,24 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
+        <div className="hero"></div>
         <div>
           <h1>Get started</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            Edit <code>src/App.jsx</code> and save to test <code>****</code>
           </p>
         </div>
         <button
@@ -116,7 +112,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(PassThrough.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
 app.use("/api", (req, res) => {
-  res.send("Hello, World");
+  res.send("Hello, World!");
 });
